3f0c0c07aff76e97dad992a755efbe9425292e2c
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-07 14:10:13 +0200

    code with then syntax

--- index.js
+++ index.js
@@ -1,99 +1,114 @@
 const getRand = (from, to) =>
-    Math.floor(Math.random() * (to - from + 1)) + from;
+	Math.floor(Math.random() * (to - from + 1)) + from;
 
 const startElapsedTime = () => {
-    return process.hrtime();
+	return process.hrtime();
 };
 
 const getElapsedTime = (timer) => {
-    const padLeft = (num, size) => {
-        num = num.toString();
-        while (num.length < size) num = '0' + num;
-        return num;
-    };
-    const ms = parseInt(process.hrtime(timer)[1] / 1000000);
-    const paddedMs = padLeft(ms, 3);
-    return `${process.hrtime(timer)[0]}.${paddedMs} seconds`;
+	const padLeft = (num, size) => {
+		num = num.toString();
+		while (num.length < size) num = '0' + num;
+		return num;
+	};
+	const ms = parseInt(process.hrtime(timer)[1] / 1000000);
+	const paddedMs = padLeft(ms, 3);
+	return `${process.hrtime(timer)[0]}.${paddedMs} seconds`;
 };
 
 const getEmployees = async () => {
-    return new Promise((resolve, reject) => {
-        const ms = getRand(1000, 3000);
-        const num = getRand(1, 2);
-        setTimeout(() => {
-            if (num !== 1) {
-                resolve([
-                    { name: 'Jack', age: 34 },
-                    { name: 'Angie', age: 55 }
-                ]);
-            } else {
-                reject(new Error('API could not process your request.'));
-            }
-        }, ms);
-    });
+	return new Promise((resolve, reject) => {
+		const ms = getRand(1000, 3000);
+		const num = getRand(1, 2);
+		setTimeout(() => {
+			if (num !== 1) {
+				resolve([
+					{ name: 'Jack', age: 34 },
+					{ name: 'Angie', age: 55 },
+				]);
+			} else {
+				reject(new Error('API could not process your request.'));
+			}
+		}, ms);
+	});
 };
 
 const getEvents = async () =>
-    new Promise((resolve, reject) => {
-        const ms = getRand(1000, 3000);
-        const num = getRand(1, 2);
+	new Promise((resolve, reject) => {
+		const ms = getRand(1000, 3000);
+		const num = getRand(1, 2);
 		setTimeout(() => {
 			if (num !== 1) {
 				resolve([
 					{
-						date: "2022-05-12", eventName: "Svelte Conference"
+						date: '2022-05-12',
+						eventName: 'Svelte Conference',
 					},
 					{
-						date: "2022-05-15", eventName: "NextJS Conference"
-					}
+						date: '2022-05-15',
+						eventName: 'NextJS Conference',
+					},
 				]);
 			} else {
 				reject(new Error('Conference data server is down.'));
 			}
 		}, ms);
-    });
+	});
 
 const apiDataServer = async () => {
-    const timer = startElapsedTime();
+	const timer = startElapsedTime();
 
-    const obj = {
-        employees: [],
+	const obj = {
+		employees: [],
 		events: [],
-        errors: [],
-        info: {
-            service: 'API Data Service',
-            version: 'v6.34',
-            elapsedTime: ''
-        }
-    };
-
-    try {
-        obj.employees = await getEmployees();
-    } catch (e) {
-        obj.employees = [];
-        obj.errors.push({
-            dataSource: 'employees',
-            message: e.message
-        });
-    }
+		errors: [],
+		info: {
+			service: 'API Data Service',
+			version: 'v6.34',
+			elapsedTime: '',
+		},
+	};
 
 	try {
-		obj.events = await getEvents();
-	}
-	catch (e) {
-		obj.events = [];
+		obj.employees = await getEmployees();
+	} catch (e) {
+		obj.employees = [];
 		obj.errors.push({
-			dataSource: 'events',
-			message: e.message
+			dataSource: 'employees',
+			message: e.message,
 		});
 	}
 
-    obj.info.elapsedTime = getElapsedTime(timer);
+	obj.info.elapsedTime = getElapsedTime(timer);
+	return getEvents()
+		.then((events) => {
+			obj.events = events;
+			return obj;
+		})
+		.catch((e) => {
+			obj.events = [];
+			obj.errors.push({
+				dataSource: 'employees',
+				message: e.message,
+			});
+			return obj;
+		});
 
-    return obj;
+	// try {
+	// 	obj.events = await getEvents();
+	// }
+	// catch (e) {
+	// 	obj.events = [];
+	// 	obj.errors.push({
+	// 		dataSource: 'events',
+	// 		message: e.message
+	// 	});
+	// }
+
+	// return obj;
 };
 
 (async () => {
-    const data = await apiDataServer();
-    console.log(data);
+	const data = await apiDataServer();
+	console.log(data);
 })();
